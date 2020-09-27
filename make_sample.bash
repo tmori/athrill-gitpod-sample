791c9ae5f94375f6c19b17919b28b2bb3acaf6ae
tmori@esm.co.jp <tmori@esm.co.jp>
2020-09-27 09:24:33 +0900

    add make

--- make_sample.bash
+++ make_sample.bash
@@ -10,4 +10,4 @@ fi
 mkdir sample1 ; cd sample1
 cp $HAKONIWA_HOME/athrill-target-rh850f1x/params/rh850f1k/atk2-sc1/*.txt .
 ../toppers-atk2-sc1/configure -T hsbrh850f1k_gcc -g ${HAKONIWA_HOME}/cfg
-
+make
